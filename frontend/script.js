480087e47bf1c89b0817f4400137e6dd18fdd948
Orji Michael Chukwuebuka <orjimichael@4886@gmail.com>
2024-09-26 16:30:21 +0100

    Add paddle mouse drag and drop

--- frontend/script.js
+++ frontend/script.js
@@ -11,31 +11,41 @@ let ballX = 292.5;
 let ballY = 192.5;
 let ballSpeedX = 2;
 let ballSpeedY = 2;
+let speedIncrease = 0.1; // Speed increment after each paddle hit
 let score1 = 0;
 let score2 = 0;
 
-// Control paddles using the keyboard
-document.addEventListener('keydown', (e) => {
-  const paddleSpeed = 20;
-  if (e.key === 'w' && paddle1Y > 0) {
-    paddle1Y -= paddleSpeed;
+// Make paddles draggable
+let isDragging1 = false;
+let isDragging2 = false;
+
+// Paddle 1 drag events
+paddle1.addEventListener('mousedown', () => (isDragging1 = true));
+document.addEventListener('mouseup', () => (isDragging1 = false));
+document.addEventListener('mousemove', (e) => {
+  if (isDragging1) {
+    let newY = e.clientY - gameArea.offsetTop - paddle1.offsetHeight / 2;
+    if (newY < 0) newY = 0;
+    if (newY > gameArea.clientHeight - paddle1.offsetHeight)
+      newY = gameArea.clientHeight - paddle1.offsetHeight;
+    paddle1Y = newY;
+    paddle1.style.top = paddle1Y + 'px';
   }
-  if (e.key === 's' && paddle1Y < gameArea.clientHeight - paddle1.offsetHeight) {
-    paddle1Y += paddleSpeed;
-  }
-  if (e.key === 'ArrowUp' && paddle2Y > 0) {
-    paddle2Y -= paddleSpeed;
-  }
-  if (e.key === 'ArrowDown' && paddle2Y < gameArea.clientHeight - paddle2.offsetHeight) {
-    paddle2Y += paddleSpeed;
-  }
-  updatePaddles();
 });
 
-function updatePaddles() {
-  paddle1.style.top = paddle1Y + 'px';
-  paddle2.style.top = paddle2Y + 'px';
-}
+// Paddle 2 drag events
+paddle2.addEventListener('mousedown', () => (isDragging2 = true));
+document.addEventListener('mouseup', () => (isDragging2 = false));
+document.addEventListener('mousemove', (e) => {
+  if (isDragging2) {
+    let newY = e.clientY - gameArea.offsetTop - paddle2.offsetHeight / 2;
+    if (newY < 0) newY = 0;
+    if (newY > gameArea.clientHeight - paddle2.offsetHeight)
+      newY = gameArea.clientHeight - paddle2.offsetHeight;
+    paddle2Y = newY;
+    paddle2.style.top = paddle2Y + 'px';
+  }
+});
 
 function moveBall() {
   ballX += ballSpeedX;
@@ -48,10 +58,12 @@ function moveBall() {
 
   // Ball collision with paddles
   if (ballX <= paddle1.offsetLeft + paddle1.offsetWidth && ballY >= paddle1Y && ballY <= paddle1Y + paddle1.offsetHeight) {
-    ballSpeedX = -ballSpeedX;
+    ballSpeedX = -(ballSpeedX + speedIncrease); // Increase speed and change direction
+    ballSpeedY += (ballSpeedY > 0 ? speedIncrease : -speedIncrease); // Increase speed in Y direction
   }
   if (ballX >= paddle2.offsetLeft - ball.offsetWidth && ballY >= paddle2Y && ballY <= paddle2Y + paddle2.offsetHeight) {
-    ballSpeedX = -ballSpeedX;
+    ballSpeedX = -(ballSpeedX - speedIncrease); // Increase speed and change direction
+    ballSpeedY += (ballSpeedY > 0 ? speedIncrease : -speedIncrease); // Increase speed in Y direction
   }
 
   // Ball out of bounds (score)
@@ -72,7 +84,8 @@ function moveBall() {
 function resetBall() {
   ballX = gameArea.clientWidth / 2 - ball.offsetWidth / 2;
   ballY = gameArea.clientHeight / 2 - ball.offsetHeight / 2;
-  ballSpeedX = -ballSpeedX;
+  ballSpeedX = 2 * (Math.random() > 0.5 ? 1 : -1); // Reset speed but randomize the direction
+  ballSpeedY = 2 * (Math.random() > 0.5 ? 1 : -1); // Reset Y direction too
 }
 
 function updateScore() {
